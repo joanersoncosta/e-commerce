5ac33d011a66263c6f8d6d456dcbda2693237697
Joanerson Costa <joanersonc31@gmail.com>
2024-01-30 16:09:19 -0300

    inicia o projeto

---
+++ src/main/java/br/com/siteware/SitewareECommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.siteware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitewareECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitewareECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/siteware/SitewareECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.siteware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitewareECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
