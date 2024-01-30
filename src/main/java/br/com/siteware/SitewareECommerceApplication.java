aa6380e0f42c4398679d61a586926018985b2518
Joanerson Costa <joanersonc31@gmail.com>
2024-01-30 16:43:21 -0300

    configura context web

--- src/main/java/br/com/siteware/SitewareECommerceApplication.java
+++ src/main/java/br/com/siteware/SitewareECommerceApplication.java
@@ -2,10 +2,18 @@ package br.com.siteware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController("/")
 @SpringBootApplication
 public class SitewareECommerceApplication {
 
+	@GetMapping
+	public String getHome() {
+		return "Siteware e-commerce - API Home";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SitewareECommerceApplication.class, args);
 	}
