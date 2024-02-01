d5726d20573ec793123134a1fb0cee7f9934fb4b
Joanerson Costa <joanersonc31@gmail.com>
2024-02-01 16:35:39 -0300

    implementa agregado de Produto

--- src/main/java/br/com/siteware/produto/Produto.java
+++ src/main/java/br/com/siteware/produto/Produto.java
@@ -3,15 +3,20 @@ package br.com.siteware.produto;
 import java.util.UUID;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.http.HttpStatus;
 
+import br.com.siteware.categoria.domain.Categoria;
+import br.com.siteware.handler.APIException;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class Produto {
-	
+
 	@Id
 	private UUID idProduto;
+	@NotNull
+	private Categoria categoria;
 	@NotNull
 	private String nome;
 	@NotBlank
@@ -19,14 +24,18 @@ public class Produto {
 	private String descricao;
 	@NotNull
 	private Double preco;
-	
-	public Produto(UUID idProduto, @NotNull String nome, @NotBlank @Size(max = 255, min = 3) String descricao,
-			@NotNull Double preco) {
-		super();
-		this.idProduto = idProduto;
+
+	public Produto(String categoriaRequest, String nome, String descricao, Double preco) {
+		this.idProduto = UUID.randomUUID();
+		this.categoria = retornaCategoria(categoriaRequest);
 		this.nome = nome;
 		this.descricao = descricao;
 		this.preco = preco;
+	}
+
+	private Categoria retornaCategoria(String categoria) {
+		return Categoria.validaCategoria(categoria)
+				.orElseThrow(() -> APIException.build(HttpStatus.BAD_REQUEST, "Categoria inválida, digite novamente."));
 	}
 
 }
