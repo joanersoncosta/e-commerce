85daabb87cffbc53663c544f5f6b76e6b30a912d
Joanerson Costa <joanersonc31@gmail.com>
2024-02-01 16:25:04 -0300

    implementa enum categoria

---
+++ src/main/java/br/com/siteware/categoria/domain/Categoria.java
@@ -0,0 +1,23 @@
+package br.com.siteware.categoria.domain;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum Categoria {
+	ELETRONICO("ELETRONICO"), IMOVEL("IMOVEL"), COSMETICO("COSMETICO"), PET("PET");
+	
+	private String categoria;
+	
+	Categoria(String  categoria){
+		this.categoria = categoria;
+	}
+	
+	public String getCategoria() {
+		return this.categoria;
+	}
+	
+	public static Optional<Categoria> validaCategoria(String categoria) {
+		return Arrays.stream(values()).filter(valorCorrespondente -> valorCorrespondente.getCategoria().equals(categoria.toUpperCase()))
+				.findFirst();
+	}
+}
