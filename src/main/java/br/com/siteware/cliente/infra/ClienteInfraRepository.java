b2353d2f06383c446f15dac938508311f6106ef9
Joanerson Costa <joanersonc31@gmail.com>
2024-02-01 06:39:09 -0300

    atualiza salva cliente na camada infra repository

--- src/main/java/br/com/siteware/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/siteware/cliente/infra/ClienteInfraRepository.java
@@ -1,9 +1,12 @@
 package br.com.siteware.cliente.infra;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import br.com.siteware.cliente.application.repository.ClienteRepository;
 import br.com.siteware.cliente.domain.Cliente;
+import br.com.siteware.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -16,7 +19,11 @@ public class ClienteInfraRepository implements ClienteRepository {
 	@Override
 	public Cliente salva(Cliente cliente) {
 		log.info("[start] ClienteInfraRepository - salva");
-		clienteSpringDBMongoRepository.save(cliente);
+		try {
+			clienteSpringDBMongoRepository.save(cliente);
+		}catch (DataIntegrityViolationException ex) {
+			throw APIException.build(HttpStatus.BAD_REQUEST, "Cliente já cadastrado.");
+		}
 		log.info("[finish] ClienteInfraRepository - salva");
 		return cliente;
 	}
