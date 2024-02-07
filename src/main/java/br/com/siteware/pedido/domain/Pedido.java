ff67a1add6ecde8b3286ebfbf79d624b73616b42
Joanerson Costa <joanersonc31@gmail.com>
2024-02-07 13:06:54 -0300

    implementa agregado de pedido

---
+++ src/main/java/br/com/siteware/pedido/domain/Pedido.java
@@ -0,0 +1,31 @@
+package br.com.siteware.pedido.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import br.com.siteware.pedido.domain.enuns.PedidoStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Document(collection = "Pedido")
+public class Pedido {
+
+	private UUID idPedido;
+	@Indexed
+	private UUID idCliente;
+	@NotNull
+	private PedidoStatus pedidoStatus;
+	private LocalDateTime momentoDoPedido;
+	private Double total;
+}
---
+++ src/main/java/br/com/siteware/pedido/domain/enuns/PedidoStatus.java
@@ -0,0 +1,5 @@
+package br.com.siteware.pedido.domain.enuns;
+
+public enum PedidoStatus {
+	PENDENTE, PAGO, CANCELADO;
+}
