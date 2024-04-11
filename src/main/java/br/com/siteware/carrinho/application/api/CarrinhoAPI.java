cbb0867b2337b5ecb4a05f55768eed9a372bf3fe
Joanerson Costa <joanersonc31@gmail.com>
2024-04-11 07:08:31 -0300

    adiciona anotacao do swagger operation no CarrinhoAPI

--- src/main/java/br/com/siteware/carrinho/application/api/CarrinhoAPI.java
+++ src/main/java/br/com/siteware/carrinho/application/api/CarrinhoAPI.java
@@ -18,24 +18,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping("/carrinho")
 public interface CarrinhoAPI {
 	
 	@PostMapping(path = "/adiciona")
 	@ResponseStatus(value = HttpStatus.CREATED)
+	@Operation(summary = "Adiciona produto no carrinho")
 	CarrinhoIdResponse adicionaProdutoAoCarrinho(@RequestHeader(name = "Authorization", required = true) String token, @RequestBody @Valid CarrinhoRequest carrinhoRequest);
 
 	@GetMapping(path = "/cliente/{idCliente}/busca-produtos")
 	@ResponseStatus(value = HttpStatus.OK)
+	@Operation(summary = "Lista todos os produtos no carrinho do Cliente")
 	List<CarrinhoListResponse> listaCarrinhoDoCliente(@RequestHeader(name = "Authorization", required = true) String token, @PathVariable(value = "idCliente") UUID idCliente);
 
 	@DeleteMapping(path = "/{idCarrinho}/remove")
 	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	@Operation(summary = "Remove produto do carrinho")
 	void removeCarrinho(@RequestHeader(name = "Authorization", required = true) String token, @PathVariable(value = "idCarrinho") UUID idCarrinho);
 
 	@PatchMapping(path = "/{idCarrinho}/edita")
 	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	@Operation(summary = "Edita quantidade do produto que está no carrinho")
 	void editaCarrinho(@RequestHeader(name = "Authorization", required = true) String token, @PathVariable(value = "idCarrinho") UUID idCarrinho, @PathParam(value = "quantidade") Integer quantidade);
 
 }
