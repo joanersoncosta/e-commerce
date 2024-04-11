2ff9207c1f7da9c727a90cb0319abc3ed55c54a1
Joanerson Costa <joanersonc31@gmail.com>
2024-04-11 16:29:50 -0300

    adiciona anotacao do swagger no Response, Request e API

--- src/main/java/br/com/siteware/admin/application/api/AdminRequest.java
+++ src/main/java/br/com/siteware/admin/application/api/AdminRequest.java
@@ -3,12 +3,15 @@ package br.com.siteware.admin.application.api;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class AdminRequest {
 	@NotBlank
+	@Schema(description = "Este é o E-mail do admin", example = "admin@gmail.com")
 	private String email;
+	@Schema(description = "Esta é a senha do cliente", example = "admin123")
 	@Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
 	private String senha;
 }
